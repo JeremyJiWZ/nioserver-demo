709f1bbb28a4f8311b507c36b5d1a3180d014406
hzjiwenzhong <hzjiwenzhong@corp.netease.com>
2018-01-28 21:41:11 +0800

    initial commit

---
+++ src/main/java/me/jeremy/nettyclient/NettyClient.java
@@ -0,0 +1,44 @@
+package me.jeremy.nettyclient;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import me.jeremy.nettyclient.handler.TimeClientHandler;
+
+/**
+ * @author hzjiwenzhong
+ */
+public class NettyClient {
+
+    public void connect(String host, int port) throws InterruptedException{
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8080;
+        String host = "localhost";
+        new NettyClient().connect(host, port);
+    }
+}
---
+++ src/main/java/me/jeremy/nettyclient/handler/TimeClientHandler.java
@@ -0,0 +1,35 @@
+package me.jeremy.nettyclient.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeClientHandler extends ChannelHandlerAdapter {
+    private final ByteBuf msg;
+
+    public TimeClientHandler() {
+        byte[] req = "GET TIME ".getBytes();
+        msg = Unpooled.buffer(req.length);
+        msg.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] resp = new byte[buf.readableBytes()];
+        buf.readBytes(resp);
+        String currentTime = new String(resp, "UTF-8");
+        System.out.println(currentTime);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
